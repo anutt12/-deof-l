e7f7d9e2778ecce873316df763996bfcc9ba0393
Andrew Nutt <anutt@paypal.com>
2021-10-15 12:54:52 -0700

    push/pull issues

--- src/main/java/com/odeofil/odeofil/model/Artist.java
+++ src/main/java/com/odeofil/odeofil/model/Artist.java
@@ -1,5 +1,8 @@
 package com.odeofil.odeofil.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -14,6 +17,64 @@ public class Artist {
     @Column
     private String name;
 
+    @Column
+    private String description;
 
+    @Column
+    private boolean isPublic;
 
+    @ManyToOne
+    @JoinColumn(name = "userProfile_id")
+    @JsonIgnore
+    private UserProfile userProfile;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "library_id")
+    private Library library;
+
+    public Artist() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
 }
